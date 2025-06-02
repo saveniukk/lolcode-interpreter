07e37e833766c2b7581a61e42e28bc564e8cc190
Yana <y.papish14@gmail.com>
2025-06-02 20:19:16 +0300

    add AST node definitions for LOLCODE parser

---
+++ src/ast_nodes.py
@@ -0,0 +1,54 @@
+class ASTNode:
+    pass
+
+
+class Program(ASTNode):
+    def __init__(self, statements):
+        self.statements = statements
+
+
+class VisibleStatement(ASTNode):
+    def __init__(self, expression):
+        self.expression = expression
+
+
+class Literal(ASTNode):
+    def __init__(self, value):
+        self.value = value
+
+
+class Variable(ASTNode):
+    def __init__(self, name):
+        self.name = name
+
+
+class Assignment(ASTNode):
+    def __init__(self, variable_name, value_expr):
+        self.variable_name = variable_name
+        self.value_expr = value_expr
+
+
+class BinaryOperation(ASTNode):
+    def __init__(self, operator, left, right):
+        self.operator = operator  # e.g., "SUM OF", "DIFF OF"
+        self.left = left
+        self.right = right
+
+
+class IfStatement(ASTNode):
+    def __init__(self, condition, true_block, false_block=None):
+        self.condition = condition
+        self.true_block = true_block
+        self.false_block = false_block
+
+
+class Loop(ASTNode):
+    def __init__(self, variable_name, start_expr, end_expr, body):
+        self.variable_name = variable_name
+        self.start_expr = start_expr
+        self.end_expr = end_expr
+        self.body = body
+
+
+class NoOp(ASTNode):
+    pass
